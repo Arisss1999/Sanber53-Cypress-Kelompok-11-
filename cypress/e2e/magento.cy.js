90e25d8b268bfbe323bf2d0c4c095672f9e71a8c
fzalv <youhaveamessag3@gmail.com>
2024-02-05 22:49:36 +0700

    Homepage to Cart

--- cypress/e2e/magento.cy.js
+++ cypress/e2e/magento.cy.js
@@ -1,8 +1,10 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("https://magento.softwaretestingboard.com/");
+    cy.visit(
+      "https://magento.softwaretestingboard.com/customer/account/login/"
+    );
 
-    cy.contains("Sign In").click();
+    //cy.contains("Sign In").click();
 
     //Login
     cy.get("#email").type("tivewo6152@bitofee.com");
@@ -19,5 +21,19 @@ describe("template spec", () => {
     // cy.get("#narrow-by-list2 > .items > .item").contains("Tops").click();
 
     cy.get("#narrow-by-list2").find("ol").contains("Tops").click();
+
+    //find product and click
+    cy.get("div ol li a")
+      .should("have.attr", "href")
+      .then((href) => {
+        cy.visit(href);
+      });
+
+    // choose size and color
+    cy.get("#option-label-size-143-item-167").click();
+    cy.get("#option-label-color-93-item-50").click();
+
+    // add to cart
+    cy.contains("Add to Cart").click();
   });
 });
